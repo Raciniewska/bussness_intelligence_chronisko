aa64d5a4fc8bc7d3af8e21c3386ef206a4a853bf
Raciniewska <40673647+Raciniewska@users.noreply.github.com>
2019-10-27 13:33:53 +0100

    Update generate_t2.py

--- generate_t2.py
+++ generate_t2.py
@@ -2,10 +2,14 @@ import random
 from datetime import datetime
 from datetime import timedelta
 import math
+import secrets
+import string
 
 koniec_czasu = datetime.strptime('2019-12-27', '%Y-%m-%d')
 rozpo_czasu=datetime.strptime('2019-10-27', '%Y-%m-%d')
-
+urodzenie_start = datetime.strptime('1919-10-26', '%Y-%m-%d')
+urodzenie_koniec = datetime.strptime('2001-10-26', '%Y-%m-%d')
+maile = ['gmail','onet','wp','o2']
 
 def random_date(start, end):
     delta = end - start
@@ -13,10 +17,41 @@ def random_date(start, end):
     random_second = random.randrange(int_delta)
     return start + timedelta(seconds=random_second)
 
+#KLIENT BULK
+print("Ile ma byc klientow?")
+#liczba_klientow = input()
+liczba_klientow = 100
+imiona_d = open('dane_do_generowania/imiona_damskie_polskie.txt',encoding="utf8").read().splitlines()
+imiona_m = open('dane_do_generowania/imiona_meskie_polskie.txt',encoding="utf8").read().splitlines()
+nazwiska_d = open('dane_do_generowania/nazwiska_damskie_polskie.txt',encoding="utf8").read().splitlines()
+nazwiska_m = open('dane_do_generowania/nazwiska_meskie_polskie.txt',encoding="utf8").read().splitlines()
+klient_bulk=open("bulks_t2/klient_t2.bulk", "w")
+miasta = open('dane_do_generowania/ludnosc_do_miasta.txt',encoding="utf8").read().splitlines()
+ulice = open('dane_do_generowania/ulice_polskie.txt',encoding="utf8").read().splitlines()
+liczba_starych_klientow=len(open('bulks/klient.bulk').read().splitlines())
+for id in range(liczba_klientow):
+    if random.randrange(20) % 2 == 0:
+        klient_bulk.write(str(liczba_starych_klientow+id + 1) + "|" + imiona_d[random.randint(0,len(imiona_d)-1)] + "|" + nazwiska_d[random.randint(0,len(nazwiska_d)-1)])
+    else:
+        klient_bulk.write(str(liczba_starych_klientow+id + 1) + "|" + imiona_m[random.randint(0,len(imiona_m)-1)] + "|" + nazwiska_m[random.randint(0,len(nazwiska_m)-1)])
+    klient_bulk.write("|" +miasta[random.randrange(0,len(miasta)-1,1)].split(',')[1]+"|"+random.choice(ulice)+
+    str(random.randint(1,200))+"/"+str(random.randint(1,50))+"|" + str(random.randrange(100000000, 999999999, 1)) + "|" +
+    str(random_date(urodzenie_start,urodzenie_koniec).date()) +"|"+ ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(12))
+    +"@"+maile[random.randint(0,len(maile)-1)]+".com"+"\n")
+id = 0
+klient_bulk.close()
+
+del miasta[:]
+del ulice[:]
+del imiona_d[:]
+del imiona_m[:]
+del nazwiska_d[:]
+del nazwiska_m[:]
+
 #FABRYKA BULK
 f = open("dane_do_generowania/fabryka_nazwa_i_adres_t2.txt", "r")
 liczba_fabryk = len(open('dane_do_generowania/fabryki_nazwa_i_adres.txt',encoding="utf8").read().splitlines())
-fabryka_bulk=open("bulks/fabryka_t2.bulk", "w")
+fabryka_bulk=open("bulks_t2/fabryka_t2.bulk", "w")
 id=liczba_fabryk+1
 for line in f:
     fabryka_bulk.write(str(id)+"|"+line.split(' ')[0]+"|"+" ".join(str(x) for x in line.split()[1:])+"\n")
@@ -28,7 +63,7 @@ fabryka_bulk.close()
 
 #TODO naprawa czasu produkcji
 #PRODUKT BULK
-produkt_bulk=open("bulks/produkt_t2.bulk", "w")
+produkt_bulk=open("bulks_t2/produkt_t2.bulk", "w")
 nazwy_producentow = open('dane_do_generowania/producenci.txt',encoding="utf8").read().splitlines()
 liczba_magazynow = len(open('dane_do_generowania/magazyn_nazwa_i_adres.txt',encoding="utf8").read().splitlines())
 liczba_produktow = len(open('bulks/produkt.bulk',encoding="utf8").read().splitlines())
@@ -42,18 +77,19 @@ produkt_bulk.close()
 del nazwy_producentow[:]
 
 #ZAMOWIENiE_BULK
-zamowienie_bulk=open("bulks/zamowienie_t2.bulk", "w")
+zamowienie_bulk=open("bulks_t2/zamowienie_t2.bulk", "w")
 print("Ile ma byc zamowien?")
 #liczba_zamowien = input()
 liczba_zamowien = 1000
 liczba_starych_zam=len(open('bulks/zamowienie.bulk',encoding="utf8").read().splitlines())
 nazwy_platnosci = open('dane_do_generowania/typ_platnosci.txt',encoding="utf8").read().splitlines()
-liczba_klientow = len(open('bulks/klient.bulk').read().splitlines())
+liczba_klientow = len(open('bulks_t2/klient_t2.bulk').read().splitlines())
+liczba_starych_klientow = len(open('bulks/klient.bulk').read().splitlines())
 liczba_agentow = len(open('bulks/agent.bulk').read().splitlines())
 liczba_kurierow = len(open('bulks/kurier.bulk',encoding="utf8").read().splitlines())
 for id in range(liczba_zamowien):
     zamowienie_bulk.write(str(liczba_starych_zam+1) + "|"
-                          +str(random.randint(1,liczba_klientow))+"|"
+                          +str(random.randint(1,liczba_klientow+liczba_starych_klientow))+"|"
     +str(random.randint(1,liczba_agentow))+"|"
                           +str(random.randint(1,liczba_kurierow))+"|"+
     str(random_date(rozpo_czasu, koniec_czasu).date())
@@ -71,7 +107,7 @@ def check_if_on_list(lista,produkt):
     return False
 
 #PRODUKT DO ZAMOWIENIA BULK
-prod_do_zam_bulk=open("bulks/produkt_do_zamowienia_t2.bulk", "w")
+prod_do_zam_bulk=open("bulks_t2/produkt_do_zamowienia_t2.bulk", "w")
 id_do_zam=liczba_zamowien
 for id in range(liczba_zamowien):
     produkty=[]
@@ -87,22 +123,22 @@ id=0
 prod_do_zam_bulk.close()
 
 #SPRAWA BULK
-sprawa_bulk=open("bulks/sprawa_t2.bulk", "w")
+sprawa_bulk=open("bulks_t2/sprawa_t2.bulk", "w")
 liczba_spraw=math.ceil(liczba_zamowien/10)
-szczeg_zam = open('bulks/zamowienie_t2.bulk', encoding="utf8").read().splitlines()
+szczeg_zam = open('bulks_t2/zamowienie_t2.bulk', encoding="utf8").read().splitlines()
 typ_sprawy = open('dane_do_generowania/typy_spraw.txt', encoding="utf8").read().splitlines()
 liczba_starych_zam=len(open('bulks/zamowienie.bulk',encoding="utf8").read().splitlines())
 liczba_starych_spraw=len(open('bulks/sprawa.bulk',encoding="utf8").read().splitlines())
 for id in range(liczba_spraw):
-    numer_zam=random.randint(liczba_starych_zam+1,liczba_starych_zam+1+liczba_zamowien)
-    data_pocz_zam=datetime.strptime(szczeg_zam[numer_zam-liczba_starych_zam-1].split('|')[4],'%Y-%m-%d')
+    numer_zam=random.randint(1,liczba_zamowien)
+    data_pocz_zam=datetime.strptime(szczeg_zam[numer_zam-1].split('|')[4],'%Y-%m-%d')
     data_pocz_zam = random_date(data_pocz_zam,koniec_czasu)
     roznica_czasu=(koniec_czasu-data_pocz_zam).days
     if roznica_czasu==0:
         zakonczenie=koniec_czasu
     else:
         zakonczenie=random_date(data_pocz_zam,koniec_czasu)
-    klient = szczeg_zam[numer_zam-liczba_starych_zam-1].split('|')[1]
+    klient = szczeg_zam[numer_zam-1].split('|')[1]
     #TODO sprawdzac produkt do zamowienia
     sprawa_bulk.write(str(liczba_starych_spraw+id + 1) + "|" +str(data_pocz_zam.date())+"|"+str(zakonczenie.date())+"|"+
         klient+"|"+str(random.randint(1,liczba_agentow))+"|"+str(numer_zam)+"|"+str(random.randint(1,liczba_produktow))+"|"+typ_sprawy[random.randint(0,len(typ_sprawy)-1)]+"\n")
@@ -110,11 +146,11 @@ id=0
 sprawa_bulk.close()
 del szczeg_zam[:]
 
-#TODO dodac nowych klientow
+
 #ROZMOWA BULK
-rozmowa_bulk=open("bulks/rozmowa_t2.bulk", "w")
+rozmowa_bulk=open("bulks_t2/rozmowa_t2.bulk", "w")
 print("Ile ma byc faktow?")
-szczeg_sprawy = open('bulks/sprawa_t2.bulk', encoding="utf8").read().splitlines()
+szczeg_sprawy = open('bulks_t2/sprawa_t2.bulk', encoding="utf8").read().splitlines()
 liczba_starych_rozmow=len(open('bulks/rozmowa.bulk',encoding="utf8").read().splitlines())
 #liczba_faktow = input()
 liczba_faktow = 5*liczba_spraw
@@ -123,7 +159,7 @@ for id in range(liczba_faktow):
     numer_sprawy=random.randrange(1,liczba_spraw)
     data_pocz_spr = datetime.strptime(szczeg_sprawy[numer_sprawy - 1].split('|')[1], '%Y-%m-%d')
     data_pocz_spr = random_date(data_pocz_spr, koniec_czasu)
-    rozmowa_bulk.write(str(liczba_starych_rozmow+id + 1) + "|"+str(random.randint(0,6000))+"|"+str(random.randint(1,liczba_klientow))
+    rozmowa_bulk.write(str(liczba_starych_rozmow+id + 1) + "|"+str(random.randint(0,6000))+"|"+str(random.randint(1,liczba_klientow+liczba_starych_klientow))
                        +"|"+str(random.randrange(1,liczba_agentow))+"|"+str(numer_sprawy+liczba_starych_spraw)
                        +"|"+str(data_pocz_spr)+"|"+str(data_pocz_spr+timedelta(seconds=random.randint(1,2400)))+"\n")
 id=0
