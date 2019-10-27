3a0d6291be2c7e6857ca66b00360276403ecd88d
DorotaPrzala <48283297+DorotaPrzala@users.noreply.github.com>
2019-10-27 19:02:39 +0100

    Update BULK_call_centersql.sql

--- schematy/BULK_call_centersql.sql
+++ schematy/BULK_call_centersql.sql
@@ -11,3 +11,4 @@ BULK INSERT dbo.zamowienie FROM 'D:\bulki\zamowienie.bulk' WITH (FIELDTERMINATOR
 BULK INSERT dbo.produkt FROM 'D:\bulki\produkt.bulk' WITH (FIELDTERMINATOR='|')
 BULK INSERT dbo.sprawa FROM 'D:\bulki\sprawa.bulk' WITH (FIELDTERMINATOR='|')
 BULK INSERT dbo.rozmowa FROM 'D:\bulki\rozmowa.bulk' WITH (FIELDTERMINATOR='|')
+BULK INSERT dbo.produkty_w_zamowieniu FROM 'D:\bulki\produkt_do_zamowienia.bulk' WITH (FIELDTERMINATOR='|')
